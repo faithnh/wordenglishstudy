b8332e87cf945e153587a930de124b0e41915860
faithnh <faithnh.stepmania.master@gmail.com>
2011-12-27 00:48:45 +0900

    test

---
+++ src/faithnh/study/wordenglishstudy/WordenglishstudyActivity.java
@@ -0,0 +1,15 @@
+package faithnh.study.wordenglishstudy;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordenglishstudyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+}
