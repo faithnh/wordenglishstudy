e17713dbd00fb20d29b59f03627addc78c812889
faithnh <faithnh.stepmania.master@gmail.com>
2011-12-27 17:24:32 +0900

    テスト実装

--- src/faithnh/study/wordenglishstudy/WordenglishstudyActivity.java
+++ src/faithnh/study/wordenglishstudy/WordenglishstudyActivity.java
@@ -2,14 +2,31 @@ package faithnh.study.wordenglishstudy;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class WordenglishstudyActivity extends Activity {
     /** Called when the activity is first created. */
+	Button testButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+ /*       OnClickListener mBackListener = new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent( WordenglishstudyActivity.this, WordTestActivity.class );
+                intent.setAction(Intent.ACTION_VIEW);
+                startActivity( intent );
+            }
+        };
+        testButton = (Button) findViewById(R.id.testButton);
+        testButton.setOnClickListener(mBackListener);*/
     }
+
 }
+
+
+
